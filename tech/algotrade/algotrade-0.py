c97f48b5c2a291057777e8d8cf63f6cb15d7153a
z3c0 <z3c0@21337.tech>
2021-06-03 10:46:45 +0000

    improve script

--- tech/algotrade/algotrade-0.py
+++ tech/algotrade/algotrade-0.py
@@ -9,6 +9,12 @@ EOD_API_KEY = config['eodhistoricaldata']['api_key']
 WATCHLIST_PATH = 'tech/algotrade/algotrade-0.csv'
 
 
+class Periods:
+    sma = [14, 21, 42, 64, 126, 252]
+    momentum = [3, 14, 21, 64]
+    lags = [3, 8, 21, 55]
+
+
 def download_stock_data(ticker):
     endpoint = (f'https://eodhistoricaldata.com/api/eod/{ticker}.US'
                 f'?api_token={EOD_API_KEY}'
@@ -18,11 +24,19 @@ def download_stock_data(ticker):
     return history_df
 
 
+def write_to_parquet(df, name):
+    df.to_parquet(f'data/parquet/{name}.parquet.gzip', compression='gzip')
+
+
+def read_from_parquet(name):
+    return pd.read_parquet(f'data/parquet/{name}.parquet.gzip')
+
+
 def process_stocks():
     watchlist_df = pd.read_csv(WATCHLIST_PATH)
 
     history_list = list()
-    for symbol in watchlist_df['symbol']:
+    for symbol in watchlist_df['Symbol']:
         history_df = download_stock_data(symbol)
         history_df['symbol'] = symbol
         history_list.append(history_df)
@@ -36,45 +50,23 @@ def process_stocks():
 
     # daily statistics
     daily_change = mkt_close - mkt_close.shift(1)
-    daily_direction = np.sign(daily_change)
     daily_returns = daily_change.cumsum()
-
-    # monthly statistics
-    monthly_mkt_close = mkt_close.resample('M').last()
-    monthly_change = monthly_mkt_close - monthly_mkt_close.shift(1)
-    monthly_direction = np.sign(monthly_change)
-    monthly_returns = monthly_change.cumsum()
-
-    # quarterly statistics
-    quarterly_mkt_close = mkt_close.resample('3M').last()
-    quarterly_change = quarterly_mkt_close - quarterly_mkt_close.shift(1)
-    quarterly_returns = quarterly_change.cumsum()
 
     # momentum - daily
     momentum = dict()
-    for period in [3, 21, 126, 252]:
+    for period in Periods.momentum:
         momentum[f'momentum_daily_{period}'] = \
-            daily_direction.rolling(period).sum()
-
-    # momentum - monthly
-    for period in [3, 6, 9, 12]:
-        momentum[f'momentum_monthly_{period}'] = \
-            monthly_direction.rolling(period).sum()
+            np.sign(daily_returns.rolling(period).mean())
 
     # simple moving average - daily
     simple_moving_average = dict()
-    for period in [21, 42, 126, 252]:
+    for period in Periods.sma:
         simple_moving_average[f'sma_daily_{period}'] = \
-            daily_returns.rolling(period).mean()
-
-    # simple moving average - monthly
-    for period in [3, 6, 12, 24]:
-        simple_moving_average[f'sma_monthly_{period}'] = \
-            monthly_returns.rolling(period).mean()
+            mkt_close.rolling(period).mean()
 
     # lagged returns - daily
     lagged_returns = dict()
-    for period in [3, 8, 21]:
+    for period in Periods.lags:
         lag_df = pd.DataFrame()
 
         for symbol in daily_returns.columns:
@@ -83,43 +75,18 @@ def process_stocks():
 
         lagged_returns[f'lag_day_{period}'] = lag_df
 
-    # lagged returns - monthly
-    for period in [3, 8, 21]:
-        lag_df = pd.DataFrame()
-
-        for symbol in monthly_returns.columns:
-            for lag in range(period + 1):
-                lag_df[symbol + f'_{lag}'] = monthly_returns[symbol].shift(lag)
-
-        lagged_returns[f'lag_month_{period}'] = lag_df
-
-    # lagged returns - quarterly
-    for period in [3, 8, 11, 21]:
-        lag_df = pd.DataFrame()
-
-        for symbol in quarterly_returns.columns:
-            for lag in range(period + 1):
-                lag_df[symbol + f'_{lag}'] = \
-                    quarterly_returns[symbol].shift(lag)
-
-        lagged_returns[f'lag_quarter_{period}'] = lag_df
-
-    def write_to_parquet(df, path):
-        df.to_parquet(path + '.parquet.gzip', compression='gzip')
-
-    write_to_parquet(mkt_close, 'data/market_close')
-    write_to_parquet(daily_returns, 'data/returns_daily')
-    write_to_parquet(monthly_returns, 'data/returns_monthly')
-    write_to_parquet(quarterly_returns, 'data/returns_quarterly')
+    write_to_parquet(mkt_close, 'market_close')
+    write_to_parquet(daily_returns, 'returns_daily')
 
     for key in momentum:
-        write_to_parquet(momentum[key], f'data/{key}')
+        write_to_parquet(momentum[key], key)
 
     for key in simple_moving_average:
-        write_to_parquet(simple_moving_average[key], f'data/{key}')
+        write_to_parquet(simple_moving_average[key], key)
 
     for key in lagged_returns:
-        write_to_parquet(lagged_returns[key], f'data/{key}')
+        write_to_parquet(lagged_returns[key], key)
+
 
 
 def backtest_sma_strategy():
